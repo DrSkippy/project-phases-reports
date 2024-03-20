06ea46b299b940f95ca7a79e9c1a2ea488d0f11f
Scott Hendrickson <scott@aspenmesh.io>
2024-03-20 16:08:45 -0600

    refactor module

--- reports/summary.py
+++ reports/summary.py
@@ -1,165 +1,6 @@
-# USAGE:
-#
-#   cd /Users/s.hendrickson/Documents/OneDrive - F5, Inc
-#   python ~/Working/2023-08-23_project_visibility/bin/update_summary.py
-#   cat "./Project Folders/summary.csv"
-#
 import csv
 import datetime
-import os
-from collections import defaultdict
-
-# Locations
-project_info_filename = "PROJECT_INFO.txt"
-projects_tree_root = "/Users/s.hendrickson/Documents/OneDrive - F5, Inc"
-projects_tree_project_folders = os.path.join(projects_tree_root, "Projects Folders")
-# Files
-summary_path = os.path.join(projects_tree_project_folders, "summary.csv")
-data_product_links_path = os.path.join(projects_tree_project_folders, "data_product_links.md")
-owner_views_active_path = os.path.join(projects_tree_project_folders, "owner_views_active.md")
-owner_views_commit_path = os.path.join(projects_tree_project_folders, "owner_views_commit.md")
-weekly_owner_views_active_path = os.path.join(projects_tree_project_folders, "weekly_owner_views_active.md")
-owner_views_completed_path = os.path.join(projects_tree_project_folders, "owner_views_completed.md")
-stakeholders_views_active_path = os.path.join(projects_tree_project_folders, "stakeholders_views_active.md")
-title_phase_views_path = os.path.join(projects_tree_project_folders, "phase_views.md")
-stakeholder_list_path = os.path.join(projects_tree_project_folders, "stakeholder_list.txt")
-
-DATE_FMT = "%Y-%m-%d"
-NOTES_DELIMITER = "; "
-
-"""
-These are the data elements to populate columns of the output csv for the status spreadsheet
-  All-caps items are read from the project_info_file while normal case items are derived or computed.
-"""
-project_params_dict = {
-    "Phases": None,
-    "Project": None,
-    "BUSINESS_SPONSOR": None,
-    "ANALYTICS_DS_OWNER": None,
-    "DATA_OFFICE_SPONSOR": None,
-    "MISSION_ALIGNMENT": None,
-    "T-SHIRT_SIZE": None,
-    "Project Folder": None,
-    "DATA_PRODUCT_LINK": None,
-    "NOTES": None,
-    "COMPUTED_AGE_DAYS": 0,
-    "COMPUTED_IN_PROGRESS_AGE_DAYS": 0,
-    "COMPUTED_PROJECT_START_DATE": None,
-    "COMPUTED_PROJECT_IN_PROGRESS_DATE": None,
-    "COMPUTED_PROJECT_END_DATE": None,
-    "COMMIT_JUSTIFICATION": None
-}
-
-"""
-Map columns to data elements
-  One entry for each of the project_params_dict keys
-  Maps to the column name in the output csv
-"""
-name_field_map = {
-    "Phases": "Phases",
-    "Project": "Project",
-    "Business Sponsor": "BUSINESS_SPONSOR",
-    "Analytics-DS Owner": "ANALYTICS_DS_OWNER",
-    "Data Office Sponsor": "DATA_OFFICE_SPONSOR",
-    "Mission Alignment": "MISSION_ALIGNMENT",
-    "T-shirt Size": "T-SHIRT_SIZE",
-    "Project Folder": "Project Folder",
-    "Data Product Link": "DATA_PRODUCT_LINK",
-    "Notes": "NOTES",
-    "Age": "COMPUTED_AGE_DAYS",
-    "In Progress Age": "COMPUTED_IN_PROGRESS_AGE_DAYS",
-    "Project Start Date": "COMPUTED_PROJECT_START_DATE",
-    "Project In-Progress Date": "COMPUTED_PROJECT_IN_PROGRESS_DATE",
-    "Project End Date": "COMPUTED_PROJECT_END_DATE",
-    "Commit Justification": "COMMIT_JUSTIFICATION"
-}
-# keep a reverse map for lookup
-field_name_map = {v: k for k, v in name_field_map.items()}
-
-# phase-dependent attributes
-project_phases = {
-    "0-Ideas": 0,
-    "1-Chartering": 1,
-    "2-Committed": 2,
-    "3-In Progress": 3,
-    "4-On Hold": 4,
-    "5-Rollout": 5,
-    "6-Completed": 6,
-    "7-Maintenance": 7,
-    "9-Ad Hoc": 9  # Ad Hoc projects are treated as In Progress WRT ordering and active status
-}
-# keep a reverse map for lookup
-index_project_phases = {v: k for k, v in project_phases.items()}
-
-# Ordering determined by Data Accelerator Analysts for Owners Reports etc.
-active_projects_order = [
-    "3-In Progress",
-    "9-Ad Hoc",
-    "4-On Hold",
-    "2-Committed",
-    "1-Chartering",
-    "5-Rollout"
-]
-
-
-def extract_stakeholders(stake_str):
-    """
-    Stakeholders are comma-separated in the project_info_file
-    Extract and return a list of stakeholders
-    """
-    sh_list = stake_str.split(",")
-    sh_list = [x.strip() for x in sh_list]
-    return sh_list
-
-
-def order_strings_by_date(string_list):
-    """
-    Used for notes ordering in reports
-        This works as desired if dates are yyyy-mm-dd format
-    """
-    res = sorted(string_list, reverse=True)
-    return res
-
-
-def parse_project_info(project_info_file):
-    """
-    Parse the project information file for required elements.
-        Input is a file object.
-        Output is a list of dictionaries
-    """
-    params_dict = project_params_dict.copy()
-    for line in project_info_file:
-        if line.startswith("#"):
-            # comment lines
-            continue
-        fields = line.split(":")
-        if fields[0].strip() in params_dict:
-            # In Split on ":", so reassemble the rest of the line in case there were colons in it
-            params_dict[fields[0].strip()] = ":".join(fields[1:]).strip().strip('"')
-        elif fields[0].startswith("NOTE"):
-            if params_dict["NOTES"] is None:
-                params_dict["NOTES"] = []
-            params_dict["NOTES"].append(line.strip())  # records include date field and colon
-        elif fields[0].startswith("COMMIT_JUSTIFICATION"):
-            if params_dict["COMMIT_JUSTIFICATION"] is None:
-                params_dict["COMMIT_JUSTIFICATION"] = []
-            params_dict["COMMIT_JUSTIFICATION"].append(
-                line.split(":")[1].strip())  # lines will be concatenated in order they appear
-    # order the notes by date
-    if params_dict["NOTES"] is not None:
-        params_dict["NOTES"] = order_strings_by_date(params_dict["NOTES"])
-
-    return params_dict
-
-
-def extract_params(root):
-    """
-    Extract phase and project names for the file path.
-        Assume start one directory above "Projects Folders"
-    """
-    names = root.split("/")  # phase, project
-    assert (len(names) == 4 and names[1] == "Projects Folders")
-    return names[2], names[3]
+from collections import defaultd
 
 
 def synthesize_sharepoint_url(project_phase, project_name):
@@ -442,6 +283,7 @@ def create_summary_csv(project_records):
         # Write each project record as a row in the CSV file
         csv_writer.writerows(project_records)
 
+
 def synthesize_email(name_set):
     result = []
     for x in name_set:
@@ -471,104 +313,14 @@ def create_complete_stakeholder_list(project_records):
         stakeholders.update(extract_stakeholders(record["BUSINESS_SPONSOR"]))
 
     stakeholderlist = synthesize_email(stakeholders)
-    
+
     # Return the list of unique stakeholders
     with open(stakeholder_list_path, "w") as outfile:
         wrt = csv.writer(outfile)
         wrt.writerows(stakeholderlist)
 
 
-if __name__ == "__main__":
-    os.chdir(projects_tree_root)
-    project_records_list = []
-    projects_processed_counter = 0
-
-    # Walk the file system from the root directory
-    for root, dirs, files in os.walk(".", topdown=False):
-        # if dirs != [] or project_info_filename not in files:
-        if project_info_filename not in files:
-            continue
-        new_project_start_date = None
-        new_project_end_date = None
-        new_project_in_progress_date = None
-        # Process Project Info file
-        with open(os.path.join(root, project_info_filename), "r") as project_info_file:
-            projects_processed_counter += 1
-            phase, project = extract_params(root)  # harvest parameters from path
-            params = parse_project_info(project_info_file)
-            params["Phases"] = phase
-            params["Project"] = project
-
-            print(f"Processing file {projects_processed_counter} ({phase}: {project})")
-
-            params["Project Folder"] = synthesize_sharepoint_url(phase, project)
-            params["NOTES"] = NOTES_DELIMITER.join(params["NOTES"]) + "\n\n"
-            if isinstance(params["COMMIT_JUSTIFICATION"], list) and len(params["COMMIT_JUSTIFICATION"]) > 0:
-                # Concatenated in order they appear
-                params["COMMIT_JUSTIFICATION"] = " ".join(params["COMMIT_JUSTIFICATION"]) + "\n\n"
-            else:
-                params["COMMIT_JUSTIFICATION"] = "Commit justification required!\n\n"
-
-            # Compute derived values for timing of phases
-            project_end_date = datetime.datetime.now()  # current last day of unfinished projects
-            if project_phases[phase] >= 6:
-                # Completed projects
-                if params["COMPUTED_PROJECT_END_DATE"] is None:
-                    # First time we processed file since project phase changed to completed
-                    project_end_date = datetime.datetime.now()
-                    params["COMPUTED_PROJECT_END_DATE"] = project_end_date.strftime(DATE_FMT)
-                    new_project_end_date = project_end_date
-                else:
-                    project_end_date = datetime.datetime.strptime(
-                        params["COMPUTED_PROJECT_END_DATE"][:10],
-                        DATE_FMT)
-
-            if project_phases[phase] >= 3:
-                # In Progress projects
-                if params["COMPUTED_PROJECT_IN_PROGRESS_DATE"] is None:
-                    # First time we processed file since project phase changed to In Progress
-                    project_in_progress_date = datetime.datetime.now()
-                    params["COMPUTED_PROJECT_IN_PROGRESS_DATE"] = project_in_progress_date.strftime(DATE_FMT)
-                    new_project_in_progress_date = project_in_progress_date
-                else:
-                    project_in_progress_date = datetime.datetime.strptime(
-                        params["COMPUTED_PROJECT_IN_PROGRESS_DATE"][:10],
-                        DATE_FMT)
-                    dt_delta = project_end_date - project_in_progress_date
-                    if dt_delta.days < 0:
-                        dt_delta = datetime.timedelta(days=0)
-                    params["COMPUTED_IN_PROGRESS_AGE_DAYS"] = dt_delta.days
-
-            if project_phases[phase] >= 1:
-                # Chartering - Active projects
-                if params["COMPUTED_PROJECT_START_DATE"] is None:
-                    # First time we processed file since project phase changed to active
-                    project_start_date = datetime.datetime.now()
-                    params["COMPUTED_PROJECT_START_DATE"] = project_start_date.strftime(DATE_FMT)
-                    new_project_start_date = project_start_date
-                else:
-                    project_start_date = datetime.datetime.strptime(
-                        params["COMPUTED_PROJECT_START_DATE"][:10],
-                        DATE_FMT)
-                    dt_delta = project_end_date - project_start_date
-                    params["COMPUTED_AGE_DAYS"] = dt_delta.days
-
-            project_records_list.append(params)
-
-        # Update the project info file with time values for phase transitions
-        if new_project_start_date is not None:
-            with open(os.path.join(root, project_info_filename), "a") as project_info_file:
-                project_info_file.write(f'COMPUTED_PROJECT_START_DATE: {new_project_start_date.strftime(DATE_FMT)}\n')
-
-        if new_project_in_progress_date is not None:
-            with open(os.path.join(root, project_info_filename), "a") as project_info_file:
-                project_info_file.write(
-                    f'COMPUTED_PROJECT_IN_PROGRESS_DATE: {new_project_in_progress_date.strftime(DATE_FMT)}\n')
-
-        if new_project_end_date is not None:
-            with open(os.path.join(root, project_info_filename), "a") as project_info_file:
-                project_info_file.write(f'COMPUTED_PROJECT_END_DATE: {new_project_end_date.strftime(DATE_FMT)}\n')
-
+def create_reports(project_records_list):
     # Create reports
     create_summary_csv(project_records_list)
     create_data_product_links(project_records_list)

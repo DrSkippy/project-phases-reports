00e79c4933ab5cceb1dea9a971ceeb881de43dd8
Scott Hendrickson <scott@drskippy.net>
2025-08-20 14:09:41 -0600

    updated snapshot script

--- tests/snapshot.bash
+++ tests/snapshot.bash
@@ -1,10 +1,13 @@
-
-dt=$(date +%Y-%m-%d:%H%M)
-echo $dt
+echo "Creating a new project snapshot tarball"
+dt=$(date +%Y-%m-%d_%H%M)
+echo "Started at $dt"
 src=/Users/s.hendrickson/Documents/OneDrive\ -\ F5,\ Inc/Projects\ Folders/
 dest=/Users/s.hendrickson/Working/project-phases-reports/tests/projects_snapshot/
 cd "${dest}"
-read -p "Warning-Deleting everything in ${dest}"
+echo "*********************************************"
+echo "Warning-Deleting everything in ${dest}"
+read -p "Hit enter to continue or Ctrl-C to abort"
+echo "*********************************************"
 rm -r *
 cd "${src}"
 echo "Creating new snapshot..."
@@ -31,3 +34,7 @@ for phases in *; do
   done
   cd ..
 done
+cd ..
+echo "Creating tarball..."
+tar -cvzf ./projects_snapshot.tar.gz projects_snapshot
+echo "Creation complete"
